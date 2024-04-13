6e6cc00adb906f05013afb361a8371df8d58e6b0
Tof <tof.aliev@ru.axept.com>
2024-04-13 20:51:53 +0500

    feature: Django lesson 13.04.2024 Part 1

--- account/models.py
+++ account/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from rest_framework import status
 from django.utils.crypto import get_random_string
-from django.contrib.auth.models import AbstractBaseUser
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(
+        self,
+        email: str | None,
+        password: str | None,
+        **extra_fields
+    ):
+        if not email:
+            raise ValidationError(
+                detail="Email not provided",
+                code=status.HTTP_400_BAD_REQUEST
+            )
+
 
 
 class CustomUser(AbstractBaseUser):
@@ -12,6 +29,8 @@ class CustomUser(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self) -> str:
         return f"{self.email}(ID={self.id})"
